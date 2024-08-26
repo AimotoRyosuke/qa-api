1c01d9b8e8aafaa52702409c0218fa5aa9948d61
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-26 21:35:27 +0900

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsApi
+module QaApi
   class Application < Rails::Application
     config.load_defaults 6.1
     config.autoloader = :zeitwerk
