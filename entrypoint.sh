96cbbd922c5745ea5ee7c7a47a633019c245ccdb
AimotoRyosuke <r.aimoto97vg@gmail.com>
2024-08-27 07:05:50 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ if [ ! -f .cognito/db/local_29ImIc0f.json ]; then
   cp .cognito/db/local_29ImIc0f.json.example .cognito/db/local_29ImIc0f.json
 fi
 
-bundle install --jobs 10 --retry 5
+rm -f tmp/pids/server.pid
+bundle exec rails s -p 3000 -b 0.0.0.0
